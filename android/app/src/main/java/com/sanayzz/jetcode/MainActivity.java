8aa9f236796af9fd9502ce7cbc6408c9e3d39f26
Sanay Raj <sanaysspace.15@gmail.com>
2023-10-18 11:00:46 -0700

    Initial Commit

---
+++ android/app/src/main/java/com/sanayzz/jetcode/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sanayzz.jetcode;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./main.css";
+
+const App: React.FC = () => {
+  return <div className="bg-black h-screen"></div>;
+};
+
+export default App;
